855dafd04b5e165a4a867de65b03855c3b7d28aa
user <user@user-PC>
2017-08-03 16:56:48 +0900

    微修正

--- src/pg_ex01_2/RomanNum.java
+++ src/pg_ex01_2/RomanNum.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class RomanNum {
 
 	public static void main(String[] args) {
-		//変数scannerに標準出力から受け取った数字を格納する
-		System.out.println("Input integer(1-20)!!");
+		//ローマ数字を位ごとに分割して、配列に格納する
+		String roman[][] = {{"","X","XX"},{"","I","II","III","IV","V","VI","VII","VIII","IX"}};
+
+		//変数inputに標準入力から受け取った整数を格納する
+		System.out.println("1から20までの整数を1つ入力してください");
 		Scanner scanner = new Scanner(System.in);
 		int input = Integer.parseInt(scanner.nextLine());
 
 		//0以下および21以上の数字が入力された場合は、エラーを出力する
 		if(input < 0 || 20 < input){
-			System.out.println("Conversion Error");
+			System.out.println("Error! 適切ではない値が代入されました");
 			return;
 		}
 
-		//標準入力された整数の商(=quot)と余(=rem)を定義する
+		//inputの十位と一位をそれぞれ変数に格納する
+		//quot:商、	rem:余り
 		int quot = input/10;
 		int rem = input%10;
 
-		//ローマ数字を位ごとにわけて、配列に格納する
-		String roman[][] = {{"","X","XX"},{"","I","II","III","IV","V","VI","VII","VIII","IX"}};
+		//quotとremに対応したローマ数字を取り出し、出力する
 		System.out.println(roman[0][quot]+roman[1][rem]);
 	}
 
