014f2442094c852e324f77955c436cd1e2a9a0cb
AMANO Masaru <pg2017.educator@gamil.com>
2017-08-24 06:25:01 +0900

    不要な定数を削除

--- src/pg_ex01_2/RomanNum2.java
+++ src/pg_ex01_2/RomanNum2.java
@@ -2,7 +2,6 @@ package pg_ex01_2;
 
 public class RomanNum2 {
 
-	static String inp_mes = "1から20までの整数を1つ入力してください";
 	static String err_mes = "変換できません";
 
 	//引数として受け取った数値をローマ数字に変換して返すクラス
@@ -11,12 +10,12 @@ public class RomanNum2 {
 		if (recnum < 1 || 20 < recnum){
 			return err_mes;
 		}
-		
+
 		int quot = recnum / 10;
 		int rem = recnum % 10;
-		
+
 		String[][] roman_nums = {{"","X","XX"},{"","I","II","III","IV","V","VI","VII","VIII","IX"}};
-		
+
 		return roman_nums[0][quot] + roman_nums[1][rem];
 	}
 }
